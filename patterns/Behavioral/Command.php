2f1802c4825ad0800f293c6fe74d88fa87990b86
sviter <nick>
2021-10-08 15:45:27 +0300

    Command pattern refactored

--- patterns/Behavioral/Command.php
+++ patterns/Behavioral/Command.php
@@ -1,8 +1,11 @@
 <?php
 
   /*
-    "Класс-функция"
-    Является инъекцией в абстрактный объект, на контекст предка которого реагирует функцией
+    "Класс-функция" или прослойка между отправителем и получателем
+    Инкапсулирует набор операций, которые будут выполняться больше одного раза в разных частях программы
+    Благодаря общему интерфейсу можно подменять наборы операций
+    Команда имеет получателя, вызывается отправителем
+
     Пример задачи:
       Строим город . Есть три типа строений:
         -торговый центр,
@@ -13,39 +16,115 @@
       Каждый метод execute знает что делать со строением
   */
 
-  interface Command
+  abstract class Command
   {
 
-    public function execute(Build $b);
+    /* @var Executor */
+    protected $_executor;
+
+    public function __construct(Executor $executor) {
+      $this->_executor = $executor;
+    }
+
+
+    abstract public function execute();
 
   }
 
-  class BuildCommand implements Command
+  class BuildCommand extends Command
   {
 
-    public function execute(Build $b) {
+    public function execute() {
       // возводим здание: берем материал, инструмент, строителей и строим
+      $executor = $this->_executor;
+      $executor->prepareWorkplace();
+      $executor->buildTarget();
+      $executor->cleanWorkPlace();
     }
 
   }
 
-  class DesignCommand implements Command
+  class DesignCommand extends Command
   {
 
-    public function execute(Build $b) {
+    public function execute() {
       // проектируем здание: берем наброски будущего строения и идем к архитектору
+      $executor = $this->_executor;
+      $executor->createEmptyProject();
+      $executor->design();
+      $executor->saveProject();
     }
 
   }
 
-  class SaleCommand implements Command
+  class SaleCommand extends Command
   {
 
-    public function execute(Build $b) {
+    public function execute() {
       // продаем здание
+      $executor = $this->_executor;
+      $executor->present();
+      $executor->makeDeal();
     }
 
   }
+
+
+  abstract class Executor {
+
+    abstract public function prepareWorkplace();
+    abstract public function buildTarget();
+    abstract public function cleanWorkPlace();
+    abstract public function createEmptyProject();
+    abstract public function design();
+    abstract public function saveProject();
+    abstract public function present();
+    abstract public function makeDeal();
+}
+
+  class ConcreteExecutor extends Executor {
+
+    public function prepareWorkplace() {
+      // TODO: Implement prepareWorkplace() method.
+    }
+
+
+    public function buildTarget() {
+      // TODO: Implement buildTarget() method.
+    }
+
+
+    public function cleanWorkPlace() {
+      // TODO: Implement cleanWorkPlace() method.
+    }
+
+
+    public function createEmptyProject() {
+      // TODO: Implement createEmptyProject() method.
+    }
+
+
+    public function design() {
+      // TODO: Implement design() method.
+    }
+
+
+    public function saveProject() {
+      // TODO: Implement saveProject() method.
+    }
+
+
+    public function present() {
+      // TODO: Implement present() method.
+    }
+
+
+    public function makeDeal() {
+      // TODO: Implement makeDeal() method.
+    }
+
+  }
+
 
   class Build
   {
@@ -59,29 +138,50 @@
     }
 
 
-    public function run() {
-      $this->c->execute($this);
+    public function release() {
+      $this->c->execute();
     }
 
   }
 
-  class Moll extends Build
+
+
+  class Car
   {
+
+    /* @var Command */
+    protected $c;
+
+
+    public function __construct(Command $c) {
+      $this->c = $c;
+    }
+
+
+    public function release() {
+      $this->c->execute();
+    }
 
   }
 
-  class Cottage extends Build
-  {
 
-  }
-
-  class Skyscraper extends Build
-  {
-
-  }
+  $executor = new ConcreteExecutor();
+  $command = new BuildCommand($executor);
+  (new Build($command))->release();
+  (new Fence($command))->do(); // or
 
 
-  (new Moll(new BuildCommand()))->run();
+  $executor = new ConcreteExecutor();
+  $command = new SaleCommand($executor);
+
+
+  // client
+  (new Build($command))->release();
+  (new Car($command))->release();
+
+
+
+
 
 
 
